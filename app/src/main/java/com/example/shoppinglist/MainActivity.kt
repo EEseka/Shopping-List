1e55014cb154d31e41ad5a7238b2d93b0e65ab34
E.Eseka <esekaemmanuel017@gmail.com>
2024-07-05 15:21:33 +0100

    Basic function of app works now

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.FloatingActionButton
@@ -17,6 +19,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,22 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Scaffold(
-                        floatingActionButton = {
-                            FloatingActionButton(
-                                onClick = { /*TODO*/ }
-                            ) {
-                                Icon(imageVector = Icons.Filled.AddCircle, contentDescription = "Add")
-                            }
-                        },
-                        content = { paddingValues ->
-                            Column(
-                                modifier = Modifier.fillMaxSize().padding(paddingValues),
-                                verticalArrangement = Arrangement.Center,
-                            ) {
-                            }
-                        }
-                    )
+                    ShoppingList()
                 }
             }
         }
